d287a98754f4ff5b3e7abaf1725a8230563bcda5
xinleima <1376260753@qq.com>
2019-11-26 18:41:31 +0800

    Add prod_static_url

--- bookseller/urls.py
+++ bookseller/urls.py
@@ -17,32 +17,45 @@ from django.conf.urls import *
 from . import testdb
 from . import view1
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
+
+def prod_static_url():
+    '''
+    prod 模式下的 url 适配
+    '''
+    from django.views import static
+    urlpattern = url(r'^static/(?P<path>.*)$', static.serve, {'document_root': settings.STATIC_ROOT}, name='static')
+    return urlpattern
+
+
 urlpatterns = [
     url(r'^testdb$', testdb.Insertsuper),
-    url(r'^sign_up',view1.sign_up),
-    url(r'^log_in',view1.log_in),
-    url(r'^showall',view1.showall,name='showall'),
-    url(r'^search',view1.search),
-    url(r'^comment',view1.comment),
-    url(r'^manager_register',view1.manager_register),
-    url(r'^entermanager',view1.entermanager,name='entermanager'),
-    url(r'^commonchange_info',view1.commonchange_info,name='commonchange_info'),
-    url(r'^superview_info',view1.superview_info,name='superview_info'),
-    url(r'^superchange_info',view1.superchange_info,name='superchange_info'),
-    url(r'^change_info',view1.change_info,name='change_info'),
-    url(r'^manager_delete',view1.manager_delete,name='manager_delete'),
-    url(r'^changebook_info',view1.changebook_info,name='changebook_info'),
-    url(r'^bookchange_info',view1.bookchange_info,name='bookchange_info'),
-    url(r'^book_delete',view1.book_delete,name='book_delete'),
-    url(r'^buy',view1.buy,name='buy'),
-    url(r'^payment',view1.payment,name='payment'),
-    url(r'^viewaccount3',view1.viewaccount3,name='viewaccount3'),
-    url(r'^viewaccount2',view1.viewaccount2,name='viewaccount2'),
-    url(r'^viewaccount1',view1.viewaccount1,name='viewaccount1'),
-    url(r'^purchase',view1.purchase,name='purchase'),
-    url(r'^pur_pay',view1.pur_pay,name='pur_pay'),
-    url(r'^cancel',view1.cancel,name='cancel'),
-    url(r'^load',view1.load,name='load'),
-    
+    url(r'^sign_up', view1.sign_up),
+    url(r'^log_in', view1.log_in),
+    url(r'^showall', view1.showall, name='showall'),
+    url(r'^search', view1.search),
+    url(r'^comment', view1.comment),
+    url(r'^manager_register', view1.manager_register),
+    url(r'^entermanager', view1.entermanager, name='entermanager'),
+    url(r'^commonchange_info', view1.commonchange_info, name='commonchange_info'),
+    url(r'^superview_info', view1.superview_info, name='superview_info'),
+    url(r'^superchange_info', view1.superchange_info, name='superchange_info'),
+    url(r'^change_info', view1.change_info, name='change_info'),
+    url(r'^manager_delete', view1.manager_delete, name='manager_delete'),
+    url(r'^changebook_info', view1.changebook_info, name='changebook_info'),
+    url(r'^bookchange_info', view1.bookchange_info, name='bookchange_info'),
+    url(r'^book_delete', view1.book_delete, name='book_delete'),
+    url(r'^buy', view1.buy, name='buy'),
+    url(r'^payment', view1.payment, name='payment'),
+    url(r'^viewaccount3', view1.viewaccount3, name='viewaccount3'),
+    url(r'^viewaccount2', view1.viewaccount2, name='viewaccount2'),
+    url(r'^viewaccount1', view1.viewaccount1, name='viewaccount1'),
+    url(r'^purchase', view1.purchase, name='purchase'),
+    url(r'^pur_pay', view1.pur_pay, name='pur_pay'),
+    url(r'^cancel', view1.cancel, name='cancel'),
+    url(r'^load', view1.load, name='load'),
+    prod_static_url()
+
 ]
 urlpatterns += staticfiles_urlpatterns()
